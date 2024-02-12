0e413528912e85536988b2a6201e3c7d723745df
rangulvers <malte.sohns@googlemail.com>
2024-02-12 16:11:05 +0100

    init upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NumberNest
+This easy script allows you to take any image and convert it into a draw by numbers canvas. 
+
+You can control the number of colors and the size of the canvas to fit your needs.
---
+++ main.py
@@ -0,0 +1,18 @@
+# this is the main file for the project. It allows the user to input a file and then generate a draw-by-numbers image from the input file
+# when calling the script the user will call it with arguments
+# arguments:
+# 1. input file
+# 2. output file
+# 3. number of colors
+
+
+
+import cv2
+import numpy as np
+import os
+import sys
+import time
+import random
+import math
+
+
